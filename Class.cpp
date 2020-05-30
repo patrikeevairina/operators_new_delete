6ae9a020715d6b6f3e55610e8a3d09c3a2b87fba
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-05-30 19:29:47 +0300

    Create Class.cpp

---
+++ Class.cpp
@@ -0,0 +1,61 @@
+#include "class.h"
+
+void* Class::operator new(size_t size)
+{
+    void *mem = std::malloc(size);
+    //std::cout << "new" << std::endl;
+    if (mem == nullptr)
+    {
+        std::bad_alloc e;
+       // std::cout << "memory can't be allocated" << std::endl;
+        throw e;
+    }
+    else
+        return mem;
+}
+
+void Class::operator delete(void *ptr)
+{
+    free(ptr);
+}
+
+void* Class::operator new[](size_t size)
+{
+    void *mem = std::malloc(size);
+   // std::cout << "new" << std::endl;
+    if (mem)
+        return mem;
+    else
+    {
+        std::bad_alloc e;
+        //std::cout << "memory can't be allocated" << std::endl;
+        throw e;
+    }
+}
+
+void Class::operator delete[](void *ptr)
+{
+    free(ptr);
+}
+
+void* Class::operator new(size_t size, void *ptr)
+{
+    return ptr;
+}
+
+void* Class::operator new[](size_t size, void *ptr)
+{
+    return ptr;
+}
+
+void* operator new(size_t size, const std::nothrow_t& nth) noexcept
+{
+    void *ptr = malloc(size);
+    return ptr;
+}
+
+void* operator new[](size_t size, const std::nothrow_t& nth) noexcept
+{
+    void *ptr = malloc(size);
+    return ptr;
+}
